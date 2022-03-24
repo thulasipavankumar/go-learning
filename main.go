e07ce1ffeb27905b1b69592c89e11dc4df8b0816
Pavan Kumar TULASI <pavan.kumar.tulasi@ing.com>
2022-03-24 16:17:55 +0100

    variables and print statements in go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main(){
+
+	// this is constant declaration and type will be assinged to float
+	const pi = 3.14;
+	// this is declaring a variable of int it can also be var age =30 and age will be predefined to 30
+	var age int = 30
+	// this is declaring a variable without datatype notice we use ":=" for dynamic declaring variable
+	name := "Pavan kumar tulasi"
+	fmt.Println("Hello World!");
+	fmt.Println("Your name is ",name,"age is: ",age);
+	fmt.Println(pi);
+
+}
