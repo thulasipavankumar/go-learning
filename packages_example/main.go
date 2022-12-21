a1d0b1eff551b7e97819b19799fbee9b0c2bb1a1
thulasipavankumar <thulasipavankumar@gmail.com>
2022-12-21 18:42:46 +0530

    added dependency from github module

--- packages_example/main.go
+++ packages_example/main.go
@@ -2,12 +2,13 @@ package main
 
 import "fmt"
 // Below package is located in "/usr/local/go/src/mypackages/arithmetic"
-import "mypackages/arithmetic"
+import local_arithmetic "mypackages/arithmetic"
+import git_arithmetic "github.com/thulasipavankumar/go-mod-math/arithmetic"
 func init() {
 	fmt.Println("This is init block")
 }
 func main() {
 	num:=13
 	fmt.Println("This is main block")
-	fmt.Printf("Is the number %d prime: %v\n",num,arithmetic.IsPrime(num))
+	fmt.Printf("Is the number %d prime: %v\n",num,local_arithmetic.IsPrime(num))
 }
