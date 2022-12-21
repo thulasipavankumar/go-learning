9846c82ddda2fc12d2ef96a1d497ec6321c3daf9
thulasipavankumar <thulasipavankumar@gmail.com>
2022-12-21 18:37:54 +0530

    added local package

--- packages_example/main.go
+++ packages_example/main.go
@@ -1,10 +1,13 @@
 package main
 
 import "fmt"
-
+// Below package is located in "/usr/local/go/src/mypackages/arithmetic"
+import "mypackages/arithmetic"
 func init() {
 	fmt.Println("This is init block")
 }
 func main() {
+	num:=13
 	fmt.Println("This is main block")
+	fmt.Printf("Is the number %d prime: %v\n",num,arithmetic.IsPrime(num))
 }
